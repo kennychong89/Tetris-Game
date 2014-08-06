a889ba8099acfddcde67c6456fa067293e5956b8
Kenny Chong <kennychong89@gmail.com>
2014-08-05 22:40:40 -0400

    Delete UIRegister.java

--- src/com/androidgame/model/UIRegister.java
+++
@@ -1,7 +0,0 @@
-package com.androidgame.model;
-
-public interface UIRegister {
-	public void register(TetrisUI_Listener listener);
-	public void unregister(TetrisUI_Listener listener);
-	public void notifyListeners();
-}
